0f3100ba67ae29055b0fb2f27a1d94d12c5e457b
John Glatts <johnglatts1@hotmail.com>
2024-12-04 21:00:44 -0500

    Update AOCDay4.cs

--- AOCDay4.cs
+++ AOCDay4.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+
 
 public class AOCDay4
 {
@@ -14,11 +15,6 @@ public class AOCDay4
     private char[,] the_map;
     private char[,] map_copy;
 
-    public enum DIRECTION 
-    {
-        HORIZONTAL,
-        VERTICAL
-    };
 
     public AOCDay4()
     {
@@ -32,7 +28,7 @@ public class AOCDay4
     {
         foreach (string line in File.ReadAllLines("C:\\Users\\jglatts\\Documents\\AOC24\\input_day4.txt"))
         {
-            if (line.Length > 0 && !line.Equals(" "))
+            if (line.Length > 0 && !string.IsNullOrEmpty(line))
                 inputString += line + "\n";
         }
     }
@@ -77,7 +73,7 @@ public class AOCDay4
 
     private void doCheckVert(int i, int j, string checkStr)
     {
-        if (the_map[i, j] == searchStr[0])
+        if (the_map[i, j] == checkStr[0])
         {
             int copy = i + 1;
             bool is_valid = true;
@@ -132,30 +128,191 @@ public class AOCDay4
             }
         }
 
-        // check for horiztonal matches
+        // check for horiztonal and vertical matches
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
             {
                 doCheckHorz(i, j, searchStr);
                 doCheckHorz(i, j, searchStrRev);
-            }
-        }
-
-        // check vertical matches
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
                 doCheckVert(i, j, searchStr);
                 doCheckVert(i, j, searchStrRev);
             }
         }
+        diagLookUpNorm();    
+        diagLookUpRev();    
+        printFindings();
+    }
 
-        Console.WriteLine(total);
 
+    private void diagLookUpRev()
+    {
         for (int i = 0; i < rows; i++)
         {
+            for (int j = 0; j < cols; j++)
+            {
+                if (the_map[i, j] == searchStrRev[0])
+                {
+                    int copy_i = i + 1;
+                    int copy_j = j + 1;
+                    bool is_valid = true;
+                    List<(int row, int col)> dims = new List<(int row, int col)>();
+                    dims.Add((i, j));
+                    for (int k = 1; k < searchStrRev.Length; k++)
+                    {
+                        if (copy_i >= rows || copy_j >= cols)
+                        {
+                            is_valid = false;
+                            break;
+                        }
+                        if (searchStrRev[k] != the_map[copy_i, copy_j])
+                        {
+                            is_valid = false;
+                            break;
+                        }
+                        else
+                        {
+                            dims.Add((copy_i, copy_j));
+                            copy_i++;
+                            copy_j++;
+                        }
+                    }
+                    if (is_valid)
+                    {
+                        int idx = 0;
+                        for (int k = 0; k < dims.Count; k++)
+                        {
+                            map_copy[dims[k].row, dims[k].col] = searchStrRev[idx++];
+                        }
+                        total += 1;
+                    }
+
+                    int copy_i_left = i + 1;
+                    int copy_j_left = j - 1;
+                    bool is_valid_left = true;
+                    List<(int row, int col)> dims_left = new List<(int row, int col)>();
+                    dims_left.Add((i, j));
+                    for (int k = 1; k < searchStrRev.Length; k++)
+                    {
+                        if (copy_i_left >= rows || copy_j_left < 0 || copy_j_left >= cols)
+                        {
+                            is_valid_left = false;
+                            break;
+                        }
+                        if (searchStrRev[k] != the_map[copy_i_left, copy_j_left])
+                        {
+                            is_valid_left = false;
+                            break;
+                        }
+                        else
+                        {
+                            dims_left.Add((copy_i_left, copy_j_left));
+                            copy_i_left++;
+                            copy_j_left--;
+                        }
+                    }
+                    if (is_valid_left)
+                    {
+                        int k = 0;
+                        int idx = 0;
+                        for (k = 0; k < dims_left.Count; k++)
+                        {
+                            map_copy[dims_left[k].row, dims_left[k].col] = searchStrRev[idx++];
+                        }
+                        total += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private void diagLookUpNorm()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (the_map[i, j] == searchStr[0])
+                {
+                    int copy_i = i + 1;
+                    int copy_j = j + 1;
+                    bool is_valid = true;
+                    List<(int row, int col)> dims = new List<(int row, int col)>();
+                    dims.Add((i, j));
+                    for (int k = 1; k < searchStr.Length; k++)
+                    {
+                        if (copy_i >= rows || copy_j >= cols)
+                        {
+                            is_valid = false;
+                            break;
+                        }
+                        if (searchStr[k] != the_map[copy_i, copy_j])
+                        {
+                            is_valid = false;
+                            break;
+                        }
+                        else
+                        {
+                            dims.Add((copy_i, copy_j));
+                            copy_i++;
+                            copy_j++;
+                        }
+                    }
+                    if (is_valid)
+                    {
+                        int idx = 0;
+                        for (int k = 0; k < dims.Count; k++)
+                        {
+                            map_copy[dims[k].row, dims[k].col] = searchStr[idx++];
+                        }
+                        total += 1;
+                    }
+
+                    int copy_i_left = i + 1;
+                    int copy_j_left = j - 1;
+                    bool is_valid_left = true;
+                    List<(int row, int col)> dims_left = new List<(int row, int col)>();
+                    dims_left.Add((i, j));
+                    for (int k = 1; k < searchStr.Length; k++)
+                    {
+                        if (copy_i_left >= rows || copy_j_left < 0 || copy_j_left >= cols)
+                        {
+                            is_valid_left = false;
+                            break;
+                        }
+                        if (searchStr[k] != the_map[copy_i_left, copy_j_left])
+                        {
+                            is_valid_left = false;
+                            break;
+                        }
+                        else
+                        {
+                            dims_left.Add((copy_i_left, copy_j_left));
+                            copy_i_left++;
+                            copy_j_left--;
+                        }
+                    }
+                    if (is_valid_left)
+                    {
+                        int idx = 0;
+                        for (int k = 0; k < dims_left.Count; k++)
+                        {
+                            map_copy[dims_left[k].row, dims_left[k].col] = searchStr[idx++];
+                        }
+                        total += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public void printFindings()
+    {
+        Console.WriteLine(total);
+        for (int i = 0; i < rows; i++)
+        {
+            string str = "row #" + i + ") ";
+            Console.Write(str.PadRight(15, ' '));
             for (int j = 0; j < cols; j++)
             {
                 Console.Write(map_copy[i, j]);
