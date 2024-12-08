a5eb824ec91bff4d811e1619801f468ebc786518
John Glatts <johnglatts1@hotmail.com>
2024-12-08 10:09:22 -0500

    Update AOCDay5.cs

--- AOCDay5.cs
+++ AOCDay5.cs
@@ -1,3 +1,14 @@
+/*
+ * 
+ *      need to change the dwatastructures
+ *      
+ *      broken_rules need to be alist of lists to get each line
+ *      getting ovrelapping with one list
+ *      will be easier to use with a list of lists 
+ * 
+ * 
+ */
+
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -8,11 +19,13 @@ public class AOCDay5
     private string inputString;
     private Dictionary<int, List<int>> rules;
     private List<List<int>> page_updates;
+    private List<(int key, int val)> broken_rule;
 
     public AOCDay5()
     {
-        rules = new Dictionary<int, List<int>>();
+        rules = new Dictionary<int, List<int>>(); 
         page_updates = new List<List<int>>();
+        broken_rule = new List<(int key, int val)>();
         LoadInputString();
     }
 
@@ -66,9 +79,13 @@ public class AOCDay5
         int total_sum = 0;
         List<int> idx_list = new List<int>();
         List<List<int>> valid_updates = new List<List<int>>();
+        List<List<int>> valid_after_fix = new List<List<int>>();
+        List<List<int>> needs_fix = new List<List<int>>();
+        broken_rule = new List<(int key, int val)>();
         List<int> copy_pg = new List<int>();
         bool is_valid = true;
-
+        broken_rule = new List<(int key, int val)>();
+        
         foreach (var pg in page_updates)
         {
             copy_pg = new List<int>(pg);
@@ -86,6 +103,7 @@ public class AOCDay5
                     {
                         if (curr == check_rules[zz])
                         {
+                            broken_rule.Add((curr, pg[z]));
                             is_valid = false;
                             break;
                         }
@@ -93,11 +111,17 @@ public class AOCDay5
                     if (!is_valid)
                         break;
                 }
+                if (!is_valid)
+                    break;
             }
             if (is_valid)
             {
                 total++;
                 valid_updates.Add(copy_pg);
+            }
+            else 
+            { 
+                needs_fix.Add(copy_pg);
             }
         }
 
@@ -114,7 +138,135 @@ public class AOCDay5
             }
             Console.WriteLine("");
         }
-        Console.WriteLine("\n\n\nanswer: " + total_sum);
+        Console.WriteLine("\n(valid-rows) answer: " + total_sum);
+
+        Console.WriteLine("\nrows that need update: ");
+        for (int i = 0; i < needs_fix.Count; i++)
+        {
+            foreach (var sss in needs_fix[i])
+            {
+                Console.Write(sss + " ");
+            }
+            int idx_1 = -1;
+            int idx_2 = -1;
+            if (i < broken_rule.Count)
+            {
+                idx_1 = needs_fix[i].IndexOf(broken_rule[i].key);
+                idx_2 = needs_fix[i].IndexOf(broken_rule[i].val);
+                if (idx_1 < 0 || idx_2 < 0)
+                    continue;
+                int temp = needs_fix[i][idx_1];
+                needs_fix[i][idx_1] = needs_fix[i][idx_2];
+                needs_fix[i][idx_2] = temp;
+            }
+        }
+
+        Console.WriteLine("\nswapping updates: ");
+        int c = 0;
+        List<List<int>> updated = new List<List<int>>();
+        for (c = 0; c < needs_fix.Count; c++)
+        {
+            List<int> fix_copy = new List<int>(needs_fix[c]);
+            bool is_fix_vald = false;
+            while (!is_fix_vald)
+            {
+                foreach (int xx in fix_copy)
+                    Console.Write(xx + " ");
+
+                if (isValidRow(fix_copy))
+                {
+                    Console.WriteLine("\t-> is good after update");
+                    is_fix_vald = true;
+                    updated.Add(fix_copy);
+                }
+                else
+                {
+                    Console.WriteLine("\t-> is not good after update -> ("
+                                        + broken_rule[0].key + ", " +
+                                        broken_rule[0].val + ")");
+                    doRowSwap(fix_copy);
+                }
+            }
+        }
+
+        Console.WriteLine("the new valid lists are: ");
+        total = 0;
+        foreach (var vv in updated)
+        {
+            if (vv.Count % 2 != 0)
+            {
+                Console.WriteLine("adding (" + vv[vv.Count / 2] + ")");
+                total += vv[vv.Count / 2];
+            }
+            foreach(int new_ele in vv)
+            {
+                Console.Write(new_ele + " ");
+            }
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine("\nnew answer :" + total);
+    }
+
+    public bool isValidRow(List<int> pg)
+    {
+        broken_rule = new List<(int key, int val)>();
+        int total = 0;
+        bool is_valid = true;
+
+        for (int k = 0; k < pg.Count; k++)
+        {
+            int curr = pg[k];
+            is_valid = true;
+            for (int z = k + 1; z < pg.Count; z++)
+            {
+                if (!rules.ContainsKey(pg[z]))
+                    continue;
+
+                List<int> check_rules = rules[pg[z]];
+                for (int zz = 0; zz < check_rules.Count; zz++)
+                {
+                    if (curr == check_rules[zz])
+                    {
+                        broken_rule.Add((curr, pg[z]));
+                        is_valid = false;
+                        break;
+                    }
+                }
+                if (!is_valid)
+                    break;
+            }
+            if (!is_valid)
+                break;
+        }
+        if (is_valid)
+        {
+            total++;
+        }
+
+        return is_valid;
+    }
+
+    public void doRowSwap(List<int> needs_fix)
+    {
+        for (int i = 0; i < needs_fix.Count; i++)
+        {
+            Console.Write(needs_fix[i] + " ");
+            int idx_1 = -1;
+            int idx_2 = -1;
+            if (i < broken_rule.Count)
+            {
+                Console.WriteLine("\nfailed elements: " + broken_rule[i] + "\n");
+                idx_1 = needs_fix.IndexOf(broken_rule[i].key);
+                idx_2 = needs_fix.IndexOf(broken_rule[i].val);
+                if (idx_1 < 0 || idx_2 < 0)
+                    continue;
+                int temp = needs_fix[idx_1];
+                needs_fix[idx_1] = needs_fix[idx_2];
+                needs_fix[idx_2] = temp;
+                return;
+            }
+        }
     }
 
     public void Print()
